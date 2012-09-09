7ddff4da7407ac81f3d7541f2a6a3ad775b79b60
Ben Andken <ben.andken@gmail.com>
2012-09-08 21:52:39 -0400

    start of prob60.  generate primes, and pair them up

---
+++ prob60.py
@@ -0,0 +1,26 @@
+# Problem 60
+# The primes 3, 7, 109, and 673, are quite remarkable. By taking any two primes and concatenating them in any order the result will always be prime. For example, taking 7 and 109, both 7109 and 1097 are prime. The sum of these four primes, 792, represents the lowest sum for a set of four primes with this property.
+# 
+# Find the lowest sum for a set of five primes for which any two primes concatenate to produce another prime.
+# 
+
+
+def generatePrimesFrom3(n):
+    primes = [3]
+    for i in range(5,n,2):
+        for j in primes:
+            if i%j == 0:
+                break
+
+            if j*j > i:
+                primes.append(i)
+                break
+
+    return primes
+
+primes = generatePrimesFrom3(100)
+fastLookup = set(primes)
+
+for i in range(0, len(primes)):
+    for j in range(i+1, len(primes)):
+            print i, j
