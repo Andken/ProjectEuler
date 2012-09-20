25480b4995da5d5554e2b4d51ace7f7fed219ee1
Ben Andken <ben.andken@gmail.com>
2012-09-19 22:17:47 -0400

    prob62.py --- solved: 127035954683

--- prob62.py
+++ prob62.py
@@ -11,11 +11,28 @@ def numDigits(a):
 
 def cube(x): return x*x*x
 
+def isPermute(x,y):
+    return sorted(str(x)) == sorted(str(y))
 
-the_list = map(lambda x:x*x*x, range(1001))    
 
+the_list = map(lambda x:x*x*x, range(10001))    
+
+
+working_digits=0
+working_list = []
 for x in the_list:
-    print x, numDigits(x)
+    if(numDigits(x) > working_digits):
+        saved_y = 0
+        for y in working_list:
+            num_permutes = 0
+            for z in working_list:
+                if not y == z:
+                    if isPermute(y, z):
+                        num_permutes = num_permutes + 1
+            if num_permutes == 4:
+                print y
 
-
-
+        working_list = []
+        working_digits = numDigits(x)
+    
+    working_list.append(x)
