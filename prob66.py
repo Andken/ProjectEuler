89219fcd3a41787b9554fe47913f2a833ce7567b
Ben Andken <ben.andken@gmail.com>
2012-09-23 20:20:53 -0400

    prob66 .... loop through D's

--- prob66.py
+++ prob66.py
@@ -5,3 +5,11 @@ import math
 def is_square(n):
     return math.sqrt(n).is_integer()
 
+
+max_minimal_x = 0
+max_minimal_D = 0
+
+for D in range(1001):
+    if not is_square(D):        
+        print D
+
