689a21ef050e194cae9e5b33a2a1c58a5c58d271
Ben Andken <ben.andken@gmail.com>
2012-12-13 22:09:04 -0500

    prob71

---
+++ prob71.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+	long long m,n;
+	long long bestm=0, bestn=1;
+
+	for(n=1;n<=1000000; ++n)
+		{
+			m=(n*3-1)/7;
+			if(m*bestn > n*bestm)
+				{
+					bestm=m;
+					bestn=n;
+				}
+		}
+
+	printf("%lld/%lld\n", bestm, bestn);
+
+	return 0;
+}
